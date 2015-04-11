e12221cba993094d2d78517a46cb3311d85aaec0
Dan Martin <thedanmartin@gmail.com>
2015-04-11 16:19:43 -0500

    edited correct_supporters_trips_table.rb with a new migration to correct flawed migration

--- db/migrate/20150411211711_correct_supporters_trips_table.rb
+++ db/migrate/20150411211711_correct_supporters_trips_table.rb
@@ -1,4 +1,6 @@
 class CorrectSupportersTripsTable < ActiveRecord::Migration
   def change
+    rename_column :supporters_trips, :supporters_id, :supporter_id
+    rename_column :supporters_trips, :trips_id, :trip_id
   end
 end
