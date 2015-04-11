87fa91ce83e71af6a10bd863d90cd09125ed2924
Dan Martin <thedanmartin@gmail.com>
2015-04-11 12:27:40 -0500

    edited supporters_trips_table to put table definition in the right place

--- db/migrate/20150411160332_supporters_trips_table.rb
+++ db/migrate/20150411160332_supporters_trips_table.rb
@@ -1,10 +1,8 @@
 class SupportersTripsTable < ActiveRecord::Migration
   def change
+    create_table :supporters_trips, id: false do |t|
+      t.belongs_to :supporters, index: true
+      t.belongs_to :trips, index: true
+    end
   end
-
-  create_table :supporters_trips, id: false do |t|
-    t.belongs_to :supporters, index: true
-    t.belongs_to :trips, index: true
-  end
-
 end
