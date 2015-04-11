fb671a9706953466f57ba039d9463df1468165ba
Dan Martin <thedanmartin@gmail.com>
2015-04-11 11:11:06 -0500

    modified supporters_trips_table migration to handle supporters_trips

--- db/migrate/20150411160332_supporters_trips_table.rb
+++ db/migrate/20150411160332_supporters_trips_table.rb
@@ -1,4 +1,10 @@
 class SupportersTripsTable < ActiveRecord::Migration
   def change
   end
+
+  create_table :supporters_trips, id: false do |t|
+    t.belongs_to :supporters, index: true
+    t.belongs_to :trips, index: true
+  end
+
 end
