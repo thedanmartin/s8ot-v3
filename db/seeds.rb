dfe9e0d2677ed3770af8aa71375238e41917b63b
Dan Martin <thedanmartin@gmail.com>
2015-04-11 20:42:15 -0500

    corrected lowercase Ts in 'trips'

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,5 +10,5 @@ karl = Supporter.create(name: "Karl", years_traveling: "2", ejections: "0")
 steve = Supporter.create(name: "Steve", years_traveling: "10", ejections: "3")
 betsy = Supporter.create(name: "Betsy", years_traveling: "1", ejections: "3")
 
-trip = trip.create(date: "2015-02-05", location: "New York", opponent: "Red Bulls", opponent_score: "1", fire_score: "0", supporters: [karl, steve])
-trip = trip.create(date: "2015-03-05", location: "Kansas City", opponent: "Sporting KC", opponent_score: "0", fire_score: "1", supporters: [betsy, steve])
+trip = Trip.create(date: "2015-02-05", location: "New York", opponent: "Red Bulls", opponent_score: "1", fire_score: "0", supporters: [karl, steve])
+trip = Trip.create(date: "2015-03-05", location: "Kansas City", opponent: "Sporting KC", opponent_score: "0", fire_score: "1", supporters: [betsy, steve])
